f76f45414026396e2d56259086971e5ca0fb58f4
dtihonov982 <dtihonov982@gmail.com>
2017-01-24 13:12:12 +0300

    a

---
+++ painter.py
@@ -0,0 +1,47 @@
+from tkinter import Tk, Canvas
+
+class __Painter():
+    def __init__(self, root):
+        width = 500
+        height = 500
+        
+        self.canvas = Canvas(root, width=width, heigh=height)
+        self.canvas.pack()
+        
+        self.x = width / 2
+        self.y = height / 2
+
+        self.pen = False
+
+    def off(self):
+        self.pen = False
+
+    def on(self):
+        self.pen = True
+
+    def move(self, dx, dy):
+        if self.pen == True:
+            prev_x, prev_y = self.x, self.y
+            
+            self.x += dx
+            self.y += dy
+
+            self.canvas.create_line(prev_x, prev_y, self.x, self.y)
+            
+        else:
+            self.x += dx
+            self.y -= dy
+            
+   
+__root = Tk()
+__p = __Painter(__root)
+
+def off():
+    __p.off()
+def on():
+    __p.on()
+def move(dx, dy):
+    __p.move(dx, dy)
+def show():
+    __root.mainloop()
+
---
+++ test.pyw
@@ -0,0 +1,9 @@
+from painter import *
+
+on()
+move(100, 0)
+move(0, 100)
+move(-100, 0)
+move(0, -100)
+
+show()
