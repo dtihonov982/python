a04e8c453537bcc33d8dd60197e443f9107b3584
Dmitrii <dtihonov982@gmail.com>
2023-05-04 08:46:23 +0300

    Body class revision

--- NewtonPhysics/body.py
+++ NewtonPhysics/body.py
@@ -11,50 +11,66 @@ class Body:
         self.y = y
         self.vx = vx
         self.vy = vy
+        #mass
         self.m = m
+        
         self.r = r
         self.id = None
+        #acceleration
         self.ax = 0
         self.ay = 0
+        
         self.color = color
+        #trace
         self.path = Path2()
+
+    #draws circle
     def draw(self, canvas):
         self.id = canvas.create_oval(
-            self.x-self.r,
-            self.y-self.r,
-            self.x+self.r,
-            self.y+self.r,
-            fill=self.color,
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r,
+            fill = self.color,
             outline='')
+        
     def move(self, dt):
-        self.x = self.x + self.vx*dt
-        self.y = self.y + self.vy*dt
-        self.vx = self.vx + self.ax*dt
-        self.vy = self.vy + self.ay*dt
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
+        self.vx = self.vx + self.ax * dt
+        self.vy = self.vy + self.ay * dt
+        
     def move_pic(self, canvas):
         canvas.coords(self.id,
                       self.x-self.r,
                       self.y-self.r,
                       self.x+self.r,
                       self.y+self.r)
+    
     def do_path(self, canvas):
         self.path.step_on(self.x, self.y, canvas)
+        
     def get_point(self):
         return np.array([self.x, self.y], dtype=np.float32)
+    
     def set_a(self, av):
         self.ax = av[0]
         self.ay = av[1]
+        
     def get_v(self):
         return sqrt(self.vx**2+self.vy**2)
+    
     #Расстояние до другого шара    
     def len(self, body):
         return sqrt((self.x-body.x)**2+(self.y-body.y)**2)
+    
     #Проверяет, столкнулся ли шар с другим
     def hits(self, body):
         if self.len(body) > self.r+body.r:
             return False
         else:
             return True
+        
     #Угол между прямой, соединяющей два шара, и осью Ох.
     def angle(self, body):
         if self.x != body.x:
@@ -62,7 +78,7 @@ class Body:
         else:
             return 0
                 
-def transform(x, y, a):
+def rotate(x, y, a):
     new_x = x*cos(a)-y*sin(a)
     new_y = x*sin(a)+y*cos(a)
     return (new_x, new_y)  
--- NewtonPhysics/five planet gravity.py
+++ NewtonPhysics/five planet gravity.py
@@ -11,83 +11,7 @@ from application import *
 if __name__ == '__main__':
     root = Tk()
     
-    #b0 = [
-    #    Body(xc/2, yc, 0, -20), 
-    #    Body(3*xc/2, yc, 0, 20)
-    #    ]
-
-    #Орбита эллиптическая
-    b1 = [
-        Body(xc, yc, 0, 0, 250000, 20),
-        Body(xc-100, yc, 0, -60, 1, 5)
-        ]
     
-    #Орбита круглая
-    V = 100
-    R = 50
-    b2 = [
-        Body(xc, yc, 0, 0, V**2*R/G, 20, color='red'),
-        Body(xc-R, yc, 0, -V, 1, 5)
-        ]
-
-    #Система из 2 тел
-    R = 2*W/8
-    r = R/10
-    V = 100.0
-    
-    k = r/R
-    u = k*V
-    m = V**2*r*(1+k)**2/G
-    M = V**2*R*(1+k)**2/G
-    
-    b3 = [
-        Body(xc, yc, 0.0, u, M, 20.0),
-        Body(xc-R-r, yc, 0.0, -V, m, 5.0)
-        ]    
-    
-    #3 тела
-    
-    trio = [
-        Body(xc, yc, 0.0, u, M, 20.0),
-        Body(xc-R-r, yc, 0.0, -V, m, 5.0),
-        Body(xc+R, yc, 0.0, V/3, m, 5.0),
-        ]   
-    
-    #2 орбиты
-    V = 100
-    r = W/10
-    R = 3*W/8
-    m = 1
-    
-    u = V*sqrt(r/R)
-    M = V**2*r/G
-    
-    orb = [
-        Body(xc, yc, 0.0, 0.0, M, 10),
-        Body(xc+r, yc, 0.0, V, m, 2),
-        Body(xc+R, yc, 0.0, u, m, 2)
-        ]        
-    
-    L0 = 500
-    m = 0.01
-    v = 100
-    R = 10
-    d = 0
-    face_to_face = [
-        Body(xc-L0/2, yc+d, v, 0, m, R),
-        Body(xc+L0/2, yc-d, -v, 0, m, R)
-        ]
-    
-    #Гравитационный трамплин
-    V = 100
-    d = 10
-    R = 50
-    M = 10**6
-    
-    trump = [
-        Body(0, yc, V, 0, 1, 2),
-        Body(xc, yc+d+R, 0, 0, M, R)
-        ]        
     
     #Скопление
     m = 300
@@ -102,13 +26,10 @@ if __name__ == '__main__':
     
     for i in range(0, n):
         b5.append(Body(x, y, vx, vy, m, r))
-        x, y = transform(x-xc, y-yc, angle)
-        vx, vy = transform(vx, vy, angle)
+        x, y = rotate(x-xc, y-yc, angle)
+        vx, vy = rotate(vx, vy, angle)
         x += xc
         y += yc
-    
-        
-
 
     
     ex = App(root, b5)
